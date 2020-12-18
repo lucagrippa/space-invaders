213414b051899241c126095594be8c79386dcd02
grippal <lucagrip@yahoo.com>
2020-12-17 20:47:36 -0500

    getElementById****

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -617,7 +617,7 @@ function onKeyUp(e) {
 }
 
 init();
-const preObject = document.getElement('object');
+const preObject = document.getElementById('object');
 const dbRefObject = firebase.database().ref().child('object');
 dbRefObject.on('value', snap => console.log(snap.val()));
 window.addEventListener("keydown", onKeyDown);
