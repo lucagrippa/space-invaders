553cf40b6a9dd233e5d651ef562a7f4c22133884
grippal <lucagrip@yahoo.com>
2020-12-17 21:24:01 -0500

    forgot parenth

--- public/assets/js/database.js
+++ public/assets/js/database.js
@@ -1,8 +1,8 @@
 function writeUserData(teamName, time, player1Score, player2Score) {
     firebase.database().ref('scorboard/' + teamName + Date.now().set({
-            teamName: teamName,
-            time: time,
-            player1Score: player1Score,
-            player2Score: player2Score,
-        });
-    }
+        teamName: teamName,
+        time: time,
+        player1Score: player1Score,
+        player2Score: player2Score,
+    }));
+}
